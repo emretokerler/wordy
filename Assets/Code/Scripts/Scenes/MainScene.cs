46f05b7c5082ae15525cb1477d02f1c082c24ec3
Emre Tokerler <emretokerler@gmail.com>
2023-11-30 01:30:31 +0300

    Refactor: comment debug lines

--- Assets/Code/Scripts/Scenes/MainScene.cs
+++ Assets/Code/Scripts/Scenes/MainScene.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using UnityEngine;
 using Wordy.Grids;
+using Grid = Wordy.Grids.Grid;
 
 namespace Wordy.Services.Scenes
 {
@@ -13,6 +16,17 @@ namespace Wordy.Services.Scenes
         private void CreateTestGrid()
         {
             GridHelper.Instance.CreateDefaultGridView();
+            // var grid = new Grid(3, 3);
+            // Debug.Log("Horizontal");
+            // GridHelper.GetGridTraverser(grid, TraverseMethod.Horizontal).ToList().ForEach(x => Debug.Log($"x:{x.X} y:{x.Y}"));
+            // Debug.Log("Vertical");
+            // GridHelper.GetGridTraverser(grid, TraverseMethod.Vertical).ToList().ForEach(x => Debug.Log($"x:{x.X} y:{x.Y}"));
+            // Debug.Log("Left Diagonal");
+            // GridHelper.GetGridTraverser(grid, TraverseMethod.DiagonalLeft).ToList().ForEach(x => Debug.Log($"x:{x.X} y:{x.Y}"));
+            // Debug.Log("Right Diagonal");
+            // GridHelper.GetGridTraverser(grid, TraverseMethod.DiagonalRight).ToList().ForEach(x => Debug.Log($"x:{x.X} y:{x.Y}"));
         }
+
+        
     }
 }
