8715f710fc0c81880efb88165ba0ad601fe6a185
Emre Tokerler <emretokerler@gmail.com>
2023-12-07 15:40:29 +0300

    Add: empty letter to constants

--- Assets/Code/Scripts/Util/Constants.cs
+++ Assets/Code/Scripts/Util/Constants.cs
@@ -3,5 +3,6 @@ namespace Wordy.Utils
     public static class Constants 
     {
         public static readonly string MAIN_SCENE_NAME = "MainScene";
+        public static readonly char EMPTY_LETTER = ' ';
     }
 }
