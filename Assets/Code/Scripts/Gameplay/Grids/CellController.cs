0a14c847d0b696a48541897eff45caac8c9cd35b
Emre Tokerler <emretokerler@gmail.com>
2023-12-07 18:29:22 +0300

    Add: SelectionBase attribute and click events on CellController

--- Assets/Code/Scripts/Gameplay/Grids/CellController.cs
+++ Assets/Code/Scripts/Gameplay/Grids/CellController.cs
@@ -3,15 +3,36 @@ using UnityEngine;
 
 namespace Wordy.Grids
 {
+    [SelectionBase]
     public class CellController : MonoBehaviour
     {
         public Cell Cell;
         [SerializeField] private TextMeshPro letterTxt;
-        
+
         public void Init(Cell cell)
         {
             Cell = cell;
             letterTxt.text = cell.Letter.ToString();
+        }
+
+        private void HandleCellHighlighted()
+        {
+            Cell.IsHighlighted = true;
+        }
+
+        private void HandleCellUnhighlighted()
+        {
+            Cell.IsHighlighted = false;
+        }
+
+        private void OnMouseOver()
+        {
+            HandleCellHighlighted();
+        }
+
+        private void OnMouseExit()
+        {
+            HandleCellUnhighlighted();
         }
     }
 }
